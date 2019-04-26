b63b49a543b180353b16c80c706d198379811d25
DmitryKlimov <Dmitry.Klimov@moex.com>
2019-04-26 17:35:51 +0300

    Added sorces addon

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -43,5 +43,29 @@ module.exports = async ({ config, mode }) => {
         enforce: 'post'
     });
 
+    config.module.rules.push({
+        test: /\.stories\.jsx?$/,
+        loaders: [
+            {
+                loader: require.resolve('@storybook/addon-storysource/loader'),
+                options: {
+                    prettierConfig: {
+                        printWidth: 80,
+                        singleQuote: false,
+                        /*
+                        DEFAULT
+                        printWidth: 120,
+                        tabWidth: 2,
+                        bracketSpacing: true,
+                        trailingComma: 'es5',
+                        singleQuote: true
+                        */
+                    },
+                }
+            }
+        ],
+        enforce: 'pre'
+    });
+
     return config;
 };
